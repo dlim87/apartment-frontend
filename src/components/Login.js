a436a024d030c21c599387ae83026d7ec7488bd3
Daniel Lim <dlim87@gmail.com>
2018-09-20 13:29:47 -0700

    fixed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react';
 import '../css/Login.css';
+import { Redirect } from 'react-router-dom'
+import AuthService from '../services/AuthService';
 
 class Login extends Component {
   constructor(){
     super()
+    this.Auth = new AuthService()
     this.state={
-      email: '',
-      password: ''
+      loggedIn: false,
+      errors:"",
+      user:{
+        email: '',
+        password: ''
+        }
     }
   }
 
-  handleChange(e){
-    this.setState({ [e.target.name]: e.target.value })
+  handleChange=(e)=>{
+    let {user} = this.state
+    // copy event target name and value (target will be a form field)
+    let fieldName = e.target.name
+    let inputValue = e.target.value
+    console.log(inputValue, fieldName);
+    // update form object with new value from user
+    user[fieldName] = inputValue
+    this.setState({user})
   }
 
-  render() {
+
+  handleFormSubmit = (e)=>{
+    e.preventDefault()
+    console.log(e);
+    this.Auth.login(this.state)
+    .then(json => {
+     console.log("handling any errors");
+     if(json.errors) {
+       this.setState({
+         errors: json.errors
+       })
+     }
+     return json
+   })
+     .then(json => {
+       console.log(this.state.loggedIn, this.state.errors)
+        if(this.Auth.loggedIn()) this.setState({loggedIn: true})
+     })
+  }
+
+  render(){
+    let {email, password}=this.state.user
     return (
       <div className="center">
         <div className="card">
           <h1>Login</h1>
-          <form>
+          <form onSubmit={this.handleFormSubmit}>
             <input
               className="form-item"
               placeholder="email goes here..."
               name="email"
               type="text"
-              onChange={this.handleChange.bind(this)}
-              value={this.state.email}
+              onChange={this.handleChange}
+              value={email}
             />
             <input
               className="form-item"
               placeholder="Password goes here..."
               name="password"
               type="password"
-              onChange={this.handleChange.bind(this)}
-              value={this.state.password}
+              onChange={this.handleChange}
+              value={password}
             />
             <input
               className="form-submit"
@@ -42,6 +77,7 @@ class Login extends Component {
               type="submit"
             />
           </form>
+          {this.state.loggedIn && <Redirect to="/" />}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Login from './components/Login'
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path='/' component={App} />
+      <Route exact path='/login' component={Login} />
     </div>
   </Router>
 , document.getElementById('root'));
